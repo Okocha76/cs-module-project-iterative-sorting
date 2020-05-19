7f9a9d9f4b08cfb86125b98f17f0c6da7a76683d
Okocha76 <Okocha76@users.noreply.github.com>
2020-05-19 17:55:53 +0300

    Monday assignments

--- src/iterative_sorting/iterative_sorting.py
+++ src/iterative_sorting/iterative_sorting.py
@@ -1,24 +1,33 @@
 # TO-DO: Complete the selection_sort() function below
 def selection_sort(arr):
+    n = len(arr)
     # loop through n-1 elements
-    for i in range(0, len(arr) - 1):
+    for i in range(0, n - 1):
         cur_index = i
         smallest_index = cur_index
         # TO-DO: find next smallest element
         # (hint, can do in 3 loc)
-        # Your code here
 
+        for j in range(cur_index, n):
+            if arr[j] < arr[smallest_index]:
+                smallest_index = j
 
         # TO-DO: swap
-        # Your code here
+        arr[cur_index], arr[smallest_index] = arr[smallest_index], arr[cur_index]
 
     return arr
 
 
 # TO-DO:  implement the Bubble Sort function below
 def bubble_sort(arr):
-    # Your code here
-
+    n = len(arr)
+    # Loop through n elements
+    for i in range(n):
+        # Loop through elements 0 to n-i-1
+        for j in range(0, n - i - 1):
+            # Swap if element found is greater than next
+            if arr[j] > arr[j + 1]:
+                arr[j], arr[j + 1] = arr[j + 1], arr[j]
 
     return arr
 
@@ -26,6 +35,5 @@ def bubble_sort(arr):
 # STRETCH: implement the Count Sort function below
 def count_sort(arr, maximum=-1):
     # Your code here
-
 
     return arr
--- src/searching/searching.py
+++ src/searching/searching.py
@@ -1,14 +1,35 @@
 def linear_search(arr, target):
-    # Your code here
+    n = len(arr)
+    if n == 0:
+        return -1
 
+    for i in range(n):
+        if arr[i] == target:
+            return i
 
     return -1   # not found
 
 
 # Write an iterative implementation of Binary Search
 def binary_search(arr, target):
+    n = len(arr)
 
-    # Your code here
+    left = 0
+    right = n - 1
 
+    while left <= right:
+        # find the midpoint
+        mid = (left + right) // 2
+        if arr[mid] == target:
+            return mid
+        # check whether element should be on left or right side of mid
+        if arr[mid] < target:
+            # toss out left side of the array
+            # update our 'left' index
+            left = mid + 1
+        else:
+            # toss out right side of the array
+            # update our 'right' index
+            right = mid - 1
 
     return -1  # not found
